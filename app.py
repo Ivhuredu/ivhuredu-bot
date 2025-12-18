d33d0b550122c51c907a7ae88cccd0057621a50a
Ivhuredu <belovedwafaith@outlook.com>
2025-12-18 18:40:57 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import os
+
+app = Flask(__name__)
+
+@app.route("/webhook", methods=["POST"])
+def arachis_bot():
+    resp = MessagingResponse()
+    resp.message("✅ Iv bot is live and responding!")
+    return str(resp)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
